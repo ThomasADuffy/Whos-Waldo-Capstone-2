eafa114b8ffc587dfad4dfe7454a73c90afb48ce
Thomas Duffy <tommy.duffy@gmail.com>
2019-10-16 17:29:51 -0600

    added data and also models

--- src/waldo_plotter.py
+++ src/waldo_plotter.py
@@ -29,6 +29,8 @@ class WaldoPlotter():
         self.waldo = waldo
 
     def create_accuracy_loss(self, figloc):
+        '''This will plot the accuracy and loss plots for the model'''
+        
         fig, ax = plt.subplots(1, 2, figsize=(12, 9))
         ax[0].plot(self.df['accuracy'], lw=3, marker='.')
         ax[0].plot(self.df['val_accuracy'], lw=3, marker='.')
@@ -44,17 +46,30 @@ class WaldoPlotter():
         ax[1].set_xlabel('Epoch')
         ax[1].legend(['Train', 'Test'], loc='upper left')
         a = self.score_accuracy
-        ls = self.score_los
+        ls = self.score_loss
         fig.suptitle(f'Model V{self.waldo.version} Loss:{ls} Acc:{a} (on holdout)',
                      fontsize=18)
         plt.savefig(figloc)
 
     def plot_model_structure(self, savedir):
+        ''' This will plot a model structure using keras's built in viz func'''
+
         return plot_model(self.waldo.model, to_file=savedir,
                           show_shapes=True, expand_nested=True)
 
     def plot_wrong_imgs(self, savedir):
-        pass
+        batch_x, batch_y = waldo1.holdout_generator.next()
+        predict_array = (np.where(waldo2.model.predict_generator(waldo1.holdout_generator)>=.5,1,0)).reshape(1,-1)
+        y_true = batch_y.reshape(1,-1)
+        idx_arr = np.where(predict_array!=y_true)[:1]
+        fig,ax = plt.subplots(1,2,figsize=(5,5))
+        image = batch_x[idx]
+        plt.imshow(image)
+        plt.show()
+        print(idx)
+        print(waldo2.model.predict_generator(waldo2.holdout_generator)[idx])
+        print(np.where(predict_array!=y_true))
+        (np.where(self.waldo.model.predict_generator(self.waldo.holdout_generator)>=.5,1,0)).reshape(1,-1)
 
 
 if __name__ == '__main__':
